265de728af91072f697a0118032b72ca931bb1c4
Nkwachi <nkwachinwamaghinna@gmail.com>
2022-04-28 10:45:46 +0100

    no internet screen

--- lib/UI/screens/failure_screen.dart
+++ lib/UI/screens/failure_screen.dart
@@ -1,14 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../utils/moni_colors.dart';
 
 class FailureScreen extends StatelessWidget {
   const FailureScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
-        children: [
-          Text('No CLuster')
-        ],
-      );
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        Center(
+          child: Icon(
+            Icons.error,
+            size: 150.0,
+            color: MoniColors.darkDarker,
+          ),
+        ),
+        SizedBox(
+          height: 16.0,
+        ),
+        Text(
+          'We encountered an error, we\'re really sorry.',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 18.0,
+          ),
+        )
+      ],
+    );
   }
 }
--- lib/UI/screens/no_internet_screen.dart
+++ lib/UI/screens/no_internet_screen.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:moni/UI/utils/moni_colors.dart';
 
 class NoInternetScreen extends StatelessWidget {
   final GestureTapCallback? onTap;
+
   const NoInternetScreen({Key? key, this.onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        TextButton.icon(onPressed: onTap, icon: Icon(Icons.add), label: Text('Retry'))
-
+      children: const [
+        Center(
+          child: Icon(
+            Icons.signal_wifi_connected_no_internet_4_rounded,
+            size: 150.0,
+            color: MoniColors.darkDarker,
+          ),
+        ),
+        SizedBox(
+          height: 16.0,
+        ),
+        Text('It seems you don\'t have internet.',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+          fontSize: 18.0,
+        ),)
       ],
     );
   }
